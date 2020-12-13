3e8f3785932fbd9f22518ae446db8c5ec8ad036d
elliotm77 <mattcoston7@gmail.com>
2020-12-12 18:36:59 -0600

    Started Todo App for class

---
+++ todo/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          This is my first ToDo App!
+        </h1>
+        <p>Test text</p>
+        <input></input> 
+        <button>Submit</button>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
