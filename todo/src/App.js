38ede809102ab7b16aec79ab4eb55b201b80862c
Matt Coston <mattcoston7@gmail.com>
2020-12-15 21:06:13 -0600

    Working on Todo App with class components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,20 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          This is my first ToDo App!
-        </h1>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+      text: "",
+      todos: [],
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ isClicked: !this.state.isClicked });
+    console.log(this.state.isClicked);
+  };
+  handleChange = (event.target.value) => {
+    this.setState({ text: !this.state.isClicked });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>This is my first ToDo App!</h1>
         <p>Test text</p>
-        <input></input> 
-        <button>Submit</button>
-      </header>
-      
-    </div>
-  );
+        <input type="text" onChange={handleChange}></input>
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    );
+  }
 }
-
 export default App;
+// Yank out all the unnecessary code and begin building a todo app
+// Create state for your app.js with isClicked: false in it
+// Create a button and add an onClick handler that changes the value of isClicked
+// Create an input field and then a function that changes the state of text: '' to the e.target.value onChange of the input field
+// Change your onClickHandler function to push the value of text into todos: []
+// Now clear text in the same function
+// Once you have state being changed properly create an element for each of the items in todos: []
+// Now .map() over todos and show each item in the DOM. REMEMBER to give the callback function in .map() an index and provide that to each of the elements as index={index}
+// Create a button on each of the elements that uses this and when clicked removes/deletes the item from todos: []
